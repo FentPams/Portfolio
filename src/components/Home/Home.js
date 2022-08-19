3e50541d2a6d5d88200986253cee1bdb0bdd5945
FentPams <xinyifeng94@gmail.com>
2022-08-19 02:21:24 -0700

    adds home.png

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home.gif";
+import homeLogo from "../../Assets/home.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
