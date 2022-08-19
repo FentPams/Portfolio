1eae7d556bec3c373fb29660d0cce87686f11b6a
FentPams <xinyifeng94@gmail.com>
2022-08-18 23:32:54 -0700

    update some info and avator

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Rapper",
+          "LOL Platimum Player",
+          "Teacher",
         ],
         autoStart: true,
         loop: true,
